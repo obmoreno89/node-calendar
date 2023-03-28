12b6210ec0e2c8317d1707a2a2c31fc6ea6c93ff
Omar Barragan Moreno <omar@Omars-MacBook-Air.local>
2023-03-28 16:00:31 -0600

    path * added

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+app.get('*', (res, rep) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 // CRUD: eventos
 
 //Escuchar peticiones
